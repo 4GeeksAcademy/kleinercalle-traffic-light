ad29b8ffa1a69e9f6d698ddd7e84ddc2bc0a57f8
kleinercalle <kleiner2024@gmail.com>
2024-08-22 14:54:23 +0000

    traffic light

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { TrafficLight } from './TrafficLight.jsx';
+
+
+
+
+// Create your first component
+const Home = () => {
+	return (
+		<div className="text-center">
+			 <TrafficLight/> 
+		
+		</div>
+	);
+};
+
+export default Home;
